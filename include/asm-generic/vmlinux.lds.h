54c25c97f3097e3ea69e8b55f350e9cdd2dea685
fiqys <elsifycritic@gmail.com>
2026-07-24 21:41:13 +0300

    include: Import A556EXXUEDZE4 OSRC changes

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -455,14 +455,14 @@
 #endif
 
 #ifdef CONFIG_UH
-#define UH_RO_SECTION							\
+#define UH_RO_SECTION						\
 	. = ALIGN(4096);						\
-	.uh_bss       : AT(ADDR(.uh_bss) - LOAD_OFFSET) {		\
-		*(.uh_bss.page_aligned)					\
+	.uh_bss       : AT(ADDR(.uh_bss) - LOAD_OFFSET) {	\
+		*(.uh_bss.page_aligned)				\
 		*(.uh_bss)						\
 	} = 0								\
 									\
-	.uh_ro        : AT(ADDR(.uh_ro) - LOAD_OFFSET) {		\
+	.uh_ro        : AT(ADDR(.uh_ro) - LOAD_OFFSET) {	\
 		*(.rkp_ro)						\
 		*(.kdp_ro)						\
 	}
@@ -490,8 +490,8 @@
 		*(.rodata1)						\
 	}								\
 									\
-	/* uH */							\
-	UH_RO_SECTION							\
+	/* uH */						\
+	UH_RO_SECTION					\
 									\
 	/* PCI quirks */						\
 	.pci_fixup        : AT(ADDR(.pci_fixup) - LOAD_OFFSET) {	\
