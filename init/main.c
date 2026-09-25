9095fe385dc80e08386c16cc7f0b6b4217f4c875
fiqys <elsifycritic@gmail.com>
2026-09-25 20:05:10 +0300

    init: Import minimal Samsung OSRC kernel source from A556BXXUEDZE4
    Change-Id: I62f44240c7f9a2d31bfefffc15ed3ccb443ffb5d
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- init/main.c
+++ init/main.c
@@ -216,8 +216,15 @@ static bool __init obsolete_checksetup(char *line)
 				pr_warn("Parameter %s is obsolete, ignored\n",
 					p->str);
 				return true;
-			} else if (p->setup_func(line + n))
-				return true;
+			} else {
+				int ret;
+
+				memblock_memsize_set_name(p->str);
+				ret = p->setup_func(line + n);
+				memblock_memsize_unset_name();
+				if (ret)
+					return true;
+			}
 		}
 		p++;
 	} while (p < __setup_end);
@@ -755,8 +762,10 @@ static int __init do_early_param(char *param, char *val,
 		    (strcmp(param, "console") == 0 &&
 		     strcmp(p->str, "earlycon") == 0)
 		) {
+			memblock_memsize_set_name(p->str);
 			if (p->setup_func(val) != 0)
 				pr_warn("Malformed early option '%s'\n", param);
+			memblock_memsize_unset_name();
 		}
 	}
 	/* We accept everything at this stage. */
@@ -1636,6 +1645,10 @@ static noinline void __init kernel_init_freeable(void)
 		page_ext_init();
 
 	do_basic_setup();
+
+#if defined(CONFIG_KUNIT) && defined(CONFIG_UML)
+	kunit_run_all_tests();
+#endif
 
 	wait_for_initramfs();
 	console_on_rootfs();
