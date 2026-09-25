f115fa11d052a421af4ba3b77f008287dd0c0590
fiqys <elsifycritic@gmail.com>
2026-09-25 20:31:46 +0300

    sound: Import Samsung OSRC kernel source from A556BXXUEDZE4
    Change-Id: Id85cb68286074dfcb12e7144be8cb11f1088b87a
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -834,6 +834,7 @@ static int usb_audio_probe(struct usb_interface *intf,
 	int ifnum;
 	u32 id;
 
+	pr_info("%s : audio probe start!\n", __func__);
 	alts = &intf->altsetting[0];
 	ifnum = get_iface_desc(alts)->bInterfaceNumber;
 	id = USB_ID(le16_to_cpu(dev->descriptor.idVendor),
@@ -955,6 +956,7 @@ static int usb_audio_probe(struct usb_interface *intf,
 	err = try_to_register_card(chip, ifnum);
 	if (err < 0)
 		goto __error_no_register;
+	pr_info("%s : card %d is registered.\n", __func__, chip->card->number);
 
 	if (chip->quirk_flags & QUIRK_FLAG_SHARE_MEDIA_DEVICE) {
 		/* don't want to fail when snd_media_device_create() fails */
@@ -973,6 +975,7 @@ static int usb_audio_probe(struct usb_interface *intf,
 	return 0;
 
  __error:
+	pr_info("%s : card probe fail.\n", __func__);
 	/* in the case of error in secondary interface, still try to register */
 	if (chip)
 		try_to_register_card(chip, ifnum);
@@ -1003,6 +1006,7 @@ static void usb_audio_disconnect(struct usb_interface *intf)
 	if (chip == USB_AUDIO_IFACE_UNUSED)
 		return;
 
+	pr_info("%s : disconnect!\n", __func__);
 	card = chip->card;
 
 	trace_android_rvh_audio_usb_offload_disconnect(intf);
@@ -1091,6 +1095,7 @@ int snd_usb_autoresume(struct snd_usb_audio *chip)
 {
 	int i, err;
 
+	pr_info("%s : ++!\n", __func__);
 	if (atomic_read(&chip->shutdown))
 		return -EIO;
 	if (atomic_inc_return(&chip->active) != 1)
@@ -1114,6 +1119,7 @@ void snd_usb_autosuspend(struct snd_usb_audio *chip)
 {
 	int i;
 
+	pr_info("%s : --!\n", __func__);
 	if (atomic_read(&chip->shutdown))
 		return;
 	if (!atomic_dec_and_test(&chip->active))
@@ -1135,6 +1141,7 @@ static int usb_audio_suspend(struct usb_interface *intf, pm_message_t message)
 	if (chip == USB_AUDIO_IFACE_UNUSED)
 		return 0;
 
+	dev_info(&intf->dev, "suspend\n");
 	if (!chip->num_suspended_intf++) {
 		list_for_each_entry(as, &chip->pcm_list, list)
 			snd_usb_pcm_suspend(as);
@@ -1165,6 +1172,7 @@ static int usb_audio_resume(struct usb_interface *intf)
 	if (chip == USB_AUDIO_IFACE_UNUSED)
 		return 0;
 
+	dev_info(&intf->dev, "resume\n");
 	atomic_inc(&chip->active); /* avoid autopm */
 	if (chip->num_suspended_intf > 1)
 		goto out;
